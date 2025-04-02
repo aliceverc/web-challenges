7143a47e690c1bb0a18e9d74d73ce501caeb84d6
Alice Vercelli <alicevercelli13@gmail.com>
2025-04-02 19:40:53 +0200

    add completed challenge 'dark mode'

--- js-dom-and-events/js-dom-and-events_dark-mode/js/index.js
+++ js-dom-and-events/js-dom-and-events_dark-mode/js/index.js
@@ -1,3 +1,24 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+//Select dark mode button and switch body to dark
+const darkModeButton = document.querySelector("[data-js='dark-mode-button']");
+darkModeButton.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+  console.log("Switched to dark");
+});
+
+//Select light mode button and switch body to light
+const lightModeButton = document.querySelector("[data-js='light-mode-button']");
+lightModeButton.addEventListener("click", () => {
+  bodyElement.classList.remove("dark");
+  console.log("Switched to light");
+});
+
+//Select toggle mode button and switch body to light/dark
+const toggleModeButton = document.querySelector("[data-js='toggle-button']");
+toggleModeButton.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+  console.log("Switched to dark/light");
+});
