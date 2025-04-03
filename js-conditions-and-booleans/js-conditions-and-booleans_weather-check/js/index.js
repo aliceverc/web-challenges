3dfe48721f8f8b312e4bcdef66707735acc498ca
Alice Vercelli <alicevercelli13@gmail.com>
2025-04-03 15:26:17 +0200

    changes

--- js-conditions-and-booleans/js-conditions-and-booleans_weather-check/js/index.js
+++ js-conditions-and-booleans/js-conditions-and-booleans_weather-check/js/index.js
@@ -1,39 +1,39 @@
 // Change this value to test different "weather" conditions.
-const weather = "snowy"
+const weather = "snowy";
 
 switch (weather) {
-    case "sunny":
-        console.log("It's sunny!");
-        break;
-    case "rainy":
-        console.log("It's raining, don't forget your umbrella!");
-        break;
-    case "snowy":
-        console.log("It's snowing, stay warm!");
-        break;
-    default:
-        console.log("I'm not sure what the weather is like.")
+  case "sunny":
+    console.log("It's sunny!");
+    break;
+  case "rainy":
+    console.log("It's raining, don't forget your umbrella!");
+    break;
+  case "snowy":
+    console.log("It's snowing, stay warm!");
+    break;
+  default:
+    console.log("I'm not sure what the weather is like.");
 }
 
 // Adjust the "temperature" value to trigger different messages (for Part 2 only).
-const temperature = 20; 
+const temperature = 20;
 
 switch (weather) {
-    case "sunny":
-    if (temperature > 20){
-        console.log("It's sunny and warm!");
+  case "sunny":
+    if (temperature > 20) {
+      console.log("It's sunny and warm!");
     } else {
-        console.log("It's sunny, but a bit chilly!");
+      console.log("It's sunny, but a bit chilly!");
     }
     break;
-    case "rainy":
-        console.log("It's raining, don't forget your umbrella!");
-        break;
-    case "snowy":
+  case "rainy":
+    console.log("It's raining, don't forget your umbrella!");
+    break;
+  case "snowy":
     if (temperature < 0) {
-        console.log("Freezing snow!");
+      console.log("Freezing snow!");
     }
     break;
-    default:
-        console.log("I'm not sure what the weather is like.")
+  default:
+    console.log("I'm not sure what the weather is like.");
 }
