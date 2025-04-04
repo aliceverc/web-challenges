9b95d9b041ad0b2dc76908b3b62ea9e65fd17a00
Alice Vercelli <alicevercelli13@gmail.com>
2025-04-04 12:22:39 +0200

    add completed challenge 'objects & arrays'

--- js-objects-and-arrays/js-objects-and-arrays_objects-and-arrays/js/index.js
+++ js-objects-and-arrays/js-objects-and-arrays_objects-and-arrays/js/index.js
@@ -50,27 +50,27 @@ const people = [
 // EXERCISE 1
 // Set the variable `nameOfFirstPerson` to the name of the first person in the array.
 
-const nameOfFirstPerson = "change me";
+const nameOfFirstPerson = "Homer";
 
 // EXERCISE 2
 // Set the variable `homersOtherFriends` to the other friends of Homer.
 
-const homersOtherFriends = "change me";
+const homersOtherFriends = ["Barney Gumble", "Carl Carlson", "Lenny Leonard"];
 
 // EXERCISE 3
 // Set the variable `ageOfThirdPerson` to the age of the third person.
 
-const ageOfThirdPerson = "change me";
+const ageOfThirdPerson = 10;
 
 // EXERCISE 4
 // Set the variable `mothersNameOfSecondPerson` to the mother's name of the second person.
 
-const mothersNameOfSecondPerson = "change me";
+const mothersNameOfSecondPerson = "Jacqueline Bouvier";
 
 // EXERCISE 5
 // Set the variable `animalTypeOfLisasPet` to the type of animal that Lisa has as a pet.
 
-const animalTypeOfLisasPet = "change me";
+const animalTypeOfLisasPet = "dog";
 
 export {
   nameOfFirstPerson,
