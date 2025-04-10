b03da4d7c372a98c15436f6f94ad40d7248d4d1d
Alice Vercelli <alicevercelli13@gmail.com>
2025-04-10 13:15:57 +0200

    add completed challenge 'while random number'

--- js-loops/js-loops_while-random-number/index.js
+++ js-loops/js-loops_while-random-number/index.js
@@ -5,8 +5,20 @@ let count = 0;
 
 // --v-- write or modify code below this line --v--
 
-// --^-- write or modify code above this line --^--
+// run the loop as long as number <= 0.9
+while (number <= 0.9) {
+  count++; // increment count
+  number = Math.random(); // generate new random number (between 0 and 1)
+  // --^-- write or modify code above this line --^--
+  console.log(
+    `The number that ended the loop was ${number} and it took ${count} rounds to do this!`
+  );
+}
 
-console.log(
-  `The number that ended the loop was ${number} and it took ${count} rounds to do this!`
-);
+//Math.random() → gives you a new number between 0 and 1
+
+//while (number <= 0.9) → keeps looping if number is 0.1, 0.5, 0.899, etc.
+
+//count++ → tracks how many times we looped
+
+//As soon as number > 0.9, the loop stops and the message is printed
