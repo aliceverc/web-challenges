e11feee1e430dfe419cacd6802cc1d03a2b8aa94
Alice Vercelli <alicevercelli13@gmail.com>
2025-04-13 13:13:46 +0200

    add completed challenge 'filter'

--- js-array-methods/js-array-methods_filter/js/index.js
+++ js-array-methods/js-array-methods_filter/js/index.js
@@ -24,17 +24,37 @@ const cards = [
   },
 ];
 
-const onlyCardWithIdTwo = null;
+// Use filter() to create an array that contains only the card with id equal to "2".
+const onlyCardWithIdTwo = cards.filter((card) => card.id === "2");
 
-const allCardsWith3Tags = null;
+console.log("Only card with id two: ", onlyCardWithIdTwo);
 
-const allCardsThatAreNotBookmarked = null;
+// Use filter() to create an array containing only cards that have exactly 3 tags.
+const allCardsWith3Tags = cards.filter((card) => card.tags.length === 3);
 
-const allCardsWithTagsHTMLOrJSThatAreBookmarked = null;
+console.log("Only cards with three tags: ", allCardsWith3Tags);
 
-export {
-  onlyCardWithIdTwo,
-  allCardsWith3Tags,
-  allCardsThatAreNotBookmarked,
-  allCardsWithTagsHTMLOrJSThatAreBookmarked,
-};
+// Use filter() to create an array of cards where isBookmarked is false.
+const allCardsThatAreNotBookmarked = cards.filter(
+  (card) => card.isBookmarked === false
+);
+
+console.log(
+  "Only cards that are not bookmarked: ",
+  allCardsThatAreNotBookmarked
+);
+
+// Use filter() with a callback that checks two conditions:
+// 1. The card's tag include "html" and the card is bookmarked
+// 2. The card's tag include "js" and the card is bookmarked
+const allCardsWithTagsHTMLOrJSThatAreBookmarked = cards.filter((card) => {
+  return (
+    (card.tags.includes("html") && card.isBookmarked === true) ||
+    (card.tags.includes("js") && card.isBookmarked === true)
+  );
+});
+
+console.log(
+  "Only cards that with tags 'html' or 'js' and that are bookmarked : ",
+  allCardsWithTagsHTMLOrJSThatAreBookmarked
+);
