d257f02d411a837615867c1bf9a9d50cf5f3dcf2
Alice Vercelli <alicevercelli13@gmail.com>
2025-04-16 01:01:06 +0200

    add completed challenge 'destructuring arrays'

--- js-modern-syntax/js-modern-syntax_destructuring-arrays/js/index.js
+++ js-modern-syntax/js-modern-syntax_destructuring-arrays/js/index.js
@@ -8,6 +8,9 @@ Make sure to export your variable as shown in the example below to make the test
 
 const item = ["Egg", 0.25, 12];
 
+export const [name, price, quantity] = item;
+console.log(name, price, quantity);
+
 // Example: export const [ value1, value2 ] = array;
 
 /*
@@ -17,6 +20,9 @@ Use array destructuring to extract the variables `firstNameOfChris`, `lastNameOf
 
 const personChris = [12, "Chris", "Owen"];
 
+export const [ageOfChris, firstNameOfChris, lastNameOfChris] = personChris;
+console.log(ageOfChris, firstNameOfChris, lastNameOfChris);
+
 /*
 EXERCISE 3
 Use array destructuring to extract the variables `firstNameOfAlex` and `lastNameOfAlex`.
@@ -24,6 +30,9 @@ Ensure no unused variables remain.
 */
 
 const personAlex = ["Alex", 12, "Main"];
+
+export const [firstNameOfAlex, , lastNameOfAlex] = personAlex;
+console.log(firstNameOfAlex, lastNameOfAlex);
 
 /*
 EXERCISE 4
@@ -33,6 +42,9 @@ as variable called `lastName`.
 
 const students = ["Christina", "Jon", "Alexandare"];
 
+export const [, , lastName] = students;
+console.log(lastName);
+
 /*
 EXERCISE 5
 Use array destructuring to extract all names from the nested array `nestedStudents`.
@@ -40,6 +52,11 @@ Assign each name to a variable called "student1" through "student5", in the give
 */
 
 const nestedStudents = ["Chris", ["Ahmad", "Antigoni"], ["Toby", "Sam"]];
+
+export const [student1, [student2, student3], [student4, student5]] =
+  nestedStudents;
+
+console.log(student1, [student2, student3], [student4, student5]);
 
 /*
 EXERCISE 6
@@ -49,7 +66,9 @@ Keep in mind that you need to export the variable `result` to make the test work
 
 const values = [1, 6, 7, 9, 12, 5, 4];
 
-const result = add(1); // Spread values inside this function call
+export const result = add(...values); // Spread values inside this function call
+
+console.log(...values);
 
 function add(...values) {
   return values.reduce(
