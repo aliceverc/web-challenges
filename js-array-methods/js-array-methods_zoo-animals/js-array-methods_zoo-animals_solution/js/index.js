1749b51fe406daeed917b7447d137cf46c6091bf
Alice Vercelli <alicevercelli13@gmail.com>
2025-04-15 13:26:30 +0200

    add completed challenge 'zoo animals'

--- js-array-methods/js-array-methods_zoo-animals/js-array-methods_zoo-animals_solution/js/index.js
+++ js-array-methods/js-array-methods_zoo-animals/js-array-methods_zoo-animals_solution/js/index.js
@@ -4,30 +4,21 @@ import { zooAnimals } from "../utils/db.js";
 // Please implement the function 'hasAnimal' that gets an array of animals and an animal name
 // The function should return 'true' if the animal name is included in the array or 'false' if not.
 
-/*function hasAnimal(animals, animalName) {
+// PART 1:
+function hasAnimal(animals, animalName) {
   return animals.includes(animalName);
-}*/
+}
 
 // Bonus:
 // Can you modify the function so that it works case-insensitive?
 // This means it should return true, even if you search for 'sheep' or 'SHEEP' or 'sHeEp'.
 // Hint: Remember the 'map' function
 
-// This function checks if a given animal name is included in an array of animals,
-// regardless of case (i.e. case-insensitive).
-function hasAnimal(animals, animalName) {
-  
-  // Convert the search term to lowercase
-  const searchTerm = animalName.toLowerCase();
-
-  // Create a new array by mapping over the original animals array,
-  // converting each animal name to lowercase
-  const lowerCaseAnimals = animals.map((animal) => animal.toLowerCase());
-
-  // Check if the lowercased search term is included in the lowercased animals array.
-  // This returns true if found, false if not.
-  return lowerCaseAnimals.includes(searchTerm);
-}
+// PART 2:
+// function hasAnimal(animals, animalName) {
+//   const lowerCaseAnimals = animals.map((animal) => animal.toLowerCase());
+//   return lowerCaseAnimals.includes(animalName.toLowerCase());
+// }
 
 // -------------------------------------------------------------------------------------
 // ----- The following code is used for the browser preview. Please don't touch it -----
