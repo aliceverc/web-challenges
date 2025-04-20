3721018416bf8f012231a63dadaaf4478f8ad31e
Alice Vercelli <alicevercelli13@gmail.com>
2025-04-20 11:47:54 +0200

    add completed challenge 'news flash'

--- js-array-methods/js-array-methods_news-flash/index.js
+++ js-array-methods/js-array-methods_news-flash/index.js
@@ -5,12 +5,27 @@ import { checkFilteredNews, checkSortedNews } from "./utils/results.js";
 const container = document.querySelector('[data-js="card-container"]');
 
 // Part 1 - start here
-const filteredNews = news.filter(() => {
-  return true;
-});
+
+// filter() filters the news array and goes through each item in the card array. The function (card) => represents a single new article and...
+const filteredNews = news.filter(
+  (card) => card.categories.includes("politics") // ...checks its categories array: if "politics" is present in the categories array of that particular card, the function return true and the card will be included in the filtered result
+);
 
 // Part 2 - start here
-const sortedNews = filteredNews;
+const sortedNews = filteredNews.toSorted((a, b) => {
+  // toSorted() sorts the filteredNews array without mutating the original array
+  const articleA = a.body.length; // get the length of the body for article A
+  const articleB = b.body.length; // get the length of the body for article B
+  if (articleA < articleB) {
+    return -1; //  if the body length of articleA is smaller that the body length of articleB, articleA comes before articleB
+  }
+  if (articleA > articleB) {
+    return 1; // if the body length of articleA is bigger that the body length of articleB, articleB comes before articleA
+  }
+  if (articleA === articleB) {
+    return 0; // if the body lengths are equal, their order stays the same
+  }
+});
 
 sortedNews.forEach((news) => {
   const cardElement = Card(news);
