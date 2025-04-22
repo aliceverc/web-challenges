2a2485a48c4daa7ba36bf52185d2d2fb7df7887a
Alice Vercelli <alicevercelli13@gmail.com>
2025-04-22 20:55:26 +0200

    add completed challenge 'calculating the sum'

--- react-props/react-props_sum/src/App.jsx
+++ react-props/react-props_sum/src/App.jsx
@@ -1,3 +1,15 @@
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  // render the Sum component with props 'valueA' and 'valueB'
+  return <Sum valueA={8} valueB={3} />;
+}
+
+// Sum component
+function Sum({ valueA, valueB }) {
+  return (
+    // render a paragraph showing the equation and its result
+    // JSX expressions inside curly braces allow embedding JS variables and calculations
+    <p>
+      {valueA} + {valueB} = {valueA + valueB}
+    </p>
+  );
 }
