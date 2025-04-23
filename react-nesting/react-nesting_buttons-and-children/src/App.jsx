b75a3567e3f74755dee1d24fbc6959213532938f
Alice Vercelli <alicevercelli13@gmail.com>
2025-04-23 11:31:10 +0200

    add completed challenge 'buttons and children'

--- react-nesting/react-nesting_buttons-and-children/src/App.jsx
+++ react-nesting/react-nesting_buttons-and-children/src/App.jsx
@@ -3,19 +3,23 @@ import "./styles.css";
 
 export default function App() {
   return (
+    // switched <Button /> to <Button>…</Button> in App
+    // by giving each <Button> its own opening and closing tags, you can pass different text (or even other elements) as children
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>Text 1</Button>
+      <Button>Text 2</Button>
+      <Button>Text 3</Button>
+      <Button>Text 4</Button>
     </main>
   );
 }
 
-function Button() {
+// destructured children in the Button component signature
+function Button({ children }) {
+  // replaced the hard-coded “Click me!” with {children}
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }
