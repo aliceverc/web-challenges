25fbc548e87b7ef8a47c4ba3e107dfa288785991
Alice Vercelli <alicevercelli13@gmail.com>
2025-04-23 18:04:23 +0200

    add completed challenge 'counter'

--- react-state-1/react-state-1_counter/src/App.jsx
+++ react-state-1/react-state-1_counter/src/App.jsx
@@ -1,24 +1,37 @@
 import "./styles.css";
+import { useState } from "react";
 
+// declare a variable 'count' with updater 'setCount'
 export default function App() {
-  let count = 0;
+  const [count, setCount] = useState(0);
 
   return (
     <div className="container">
+      {/* display the current count */}
       <h1>{count}</h1>
+
+      {/* 
+      minus button:
+        - directly call setCount(count - 1)
+      */}
       <div className="button-container">
         <button
           type="button"
           onClick={() => {
-            console.log("🤔");
+            setCount(count - 1);
           }}
         >
           -
         </button>
+
+        {/* 
+      plus button:
+        - directly call setCount(count + 1)
+      */}
         <button
           type="button"
-          onClick={() => {
-            console.log("🤔");
+          onClick={(handleClick) => {
+            setCount(count + 1);
           }}
         >
           +
