35b03bd468a64e78525327c907835d94452f7b1b
Alice Vercelli <alicevercelli13@gmail.com>
2025-04-24 11:46:40 +0200

    challenge completed 'fruits'

--- react-with-arrays/react-with-arrays_fruits/src/components/Card.jsx
+++ react-with-arrays/react-with-arrays_fruits/src/components/Card.jsx
@@ -1,5 +1,8 @@
 import "./Card.css";
 
-export default function Card({ name }) {
-  return <p className="card">{name}</p>;
+// card component to dispaly a fruit item with dynamic background color
+export default function Card({ name, color }) {
+  // combine the base 'card' class with a modifier based on the color prop
+  // example: color = 'green' > className = "card card--green"
+  return <p className={`card card--${color}`}>{name}</p>;
 }
